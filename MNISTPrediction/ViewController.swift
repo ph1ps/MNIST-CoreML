0670563f633d44d4c930a4ce28fe2624e3f61553
ph1ps <gabriel.phi@gmx.at>
2017-06-19 10:20:13 +0200

    Add initial project

---
+++ MNISTPrediction/ViewController.swift
@@ -0,0 +1,63 @@
+//
+//  ViewController.swift
+//  MNISTPrediction
+//
+//  Created by Philipp Gabriel on 18.06.17.
+//  Copyright © 2017 Philipp Gabriel. All rights reserved.
+//
+
+import UIKit
+
+extension UIImage {
+    
+    func resize(to newSize: CGSize) -> UIImage {
+        UIGraphicsBeginImageContextWithOptions(CGSize(width: newSize.width, height: newSize.height), true, 1.0)
+        self.draw(in: CGRect(x: 0, y: 0, width: newSize.width, height: newSize.height))
+        let resizedImage = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+        
+        return resizedImage
+    }
+    
+    func buffer() -> CVPixelBuffer? {
+        var pixelBuffer: CVPixelBuffer? = nil
+        
+        let width = Int(self.size.width)
+        let height = Int(self.size.height)
+        
+        CVPixelBufferCreate(kCFAllocatorDefault, width, height, kCVPixelFormatType_OneComponent8, nil, &pixelBuffer)
+        CVPixelBufferLockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags(rawValue:0))
+        
+        let colorspace = CGColorSpaceCreateDeviceGray()
+        let bitmapContext = CGContext(data: CVPixelBufferGetBaseAddress(pixelBuffer!), width: width, height: height, bitsPerComponent: 8, bytesPerRow: CVPixelBufferGetBytesPerRow(pixelBuffer!), space: colorspace, bitmapInfo: 0)!
+        
+        bitmapContext.draw(self.cgImage!, in: CGRect(x: 0, y: 0, width: width, height: height))
+        
+        return pixelBuffer
+    }
+}
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let size = CGSize(width: 28, height: 28)
+
+        guard let result = try? MNIST().prediction(image: #imageLiteral(resourceName: "eight").resize(to: size).buffer()!) else {
+            print("failed")
+            return
+        }
+        
+        let count = result.prediction.count
+        let ptr = result.prediction.dataPointer
+        let buffer = UnsafeBufferPointer(start: ptr.assumingMemoryBound(to: Double.self), count: count)
+        let array = Array(buffer) as [Double]
+        
+        print(array.index(of: 1.0) ?? "couldn't find any prediction")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
